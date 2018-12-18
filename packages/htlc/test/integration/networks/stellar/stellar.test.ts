b042938165765ac4c961a782ff41873934369c58
leon-do <leondo123@gmail.com>
2018-12-18 15:10:19 -0700

    stellar tests complete

--- packages/htlc/test/integration/networks/stellar/stellar.test.ts
+++ packages/htlc/test/integration/networks/stellar/stellar.test.ts
@@ -23,88 +23,88 @@ describe('Stellar HTLC - Stellar Network', () => {
   const hashX =
     '087d80f7f182dd44f184aa86ca34488853ebcc04f0c60d5294919a466b463831';
 
-  // describe('Create', () => {
-  //   it('should create and broadcast a transaction envelope', async () => {
-  //     const keyPair = stellarSdk.Keypair.fromSecret(serverSecret);
-  //     // create envelope to broadcast
-  //     const { createEnvelope } = await htlc.create(keyPair);
-  //     // broadcast envelope
-  //     const txResp = await htlc.broadcast(createEnvelope);
-  //     expect(typeof txResp.hash).to.equal('string');
-  //   });
-  // });
+  describe('Create', () => {
+    it('should create and broadcast a transaction envelope', async () => {
+      const keyPair = stellarSdk.Keypair.fromSecret(serverSecret);
+      // create envelope to broadcast
+      const { createEnvelope } = await htlc.create(keyPair);
+      // broadcast envelope
+      const txResp = await htlc.broadcast(createEnvelope);
+      expect(typeof txResp.hash).to.equal('string');
+    });
+  });
 
-  // describe('Fund', () => {
-  //   it('should create escrow account, create fund envelope then broadcast', async () => {
-  //     const serverKeyPair = stellarSdk.Keypair.fromSecret(serverSecret);
-  //     // server creates envelope to broadcast
-  //     const { createEnvelope, escrowPubKey } = await htlc.create(serverKeyPair);
-  //     // broadcast envelope
-  //     await htlc.broadcast(createEnvelope);
-  //     // create fund envelope, sends to user
-  //     const fundEnvelope = await htlc.fund(
-  //       serverKeyPair,
-  //       userPubKey,
-  //       escrowPubKey,
-  //       3,
-  //       hashX,
-  //     );
-  //     // user signs fund envelope
-  //     const userKeyPair = stellarSdk.Keypair.fromSecret(userSecret);
-  //     const signedFundEnvelope = htlc.sign(userKeyPair, fundEnvelope);
-  //     // broadcast funded envelope
-  //     const fundedTxResp = await htlc.broadcast(signedFundEnvelope);
-  //     // get transaction operation
-  //     const operations = await axios.get(
-  //       `${fundedTxResp._links.transaction.href}/operations`,
-  //     );
-  //     expect(operations.data._embedded.records.length).to.equal(4);
-  //     expect(operations.data._embedded.records[0].to).to.equal(escrowPubKey);
-  //     expect(operations.data._embedded.records[0].from).to.equal(userPubKey);
-  //     expect(operations.data._embedded.records[0].amount).to.equal('3.0000000');
-  //   });
-  // });
+  describe('Fund', () => {
+    it('should create escrow account, create fund envelope then broadcast', async () => {
+      const serverKeyPair = stellarSdk.Keypair.fromSecret(serverSecret);
+      // server creates envelope to broadcast
+      const { createEnvelope, escrowPubKey } = await htlc.create(serverKeyPair);
+      // broadcast envelope
+      await htlc.broadcast(createEnvelope);
+      // create fund envelope, sends to user
+      const fundEnvelope = await htlc.fund(
+        serverKeyPair,
+        userPubKey,
+        escrowPubKey,
+        3,
+        hashX,
+      );
+      // user signs fund envelope
+      const userKeyPair = stellarSdk.Keypair.fromSecret(userSecret);
+      const signedFundEnvelope = htlc.sign(userKeyPair, fundEnvelope);
+      // broadcast funded envelope
+      const fundedTxResp = await htlc.broadcast(signedFundEnvelope);
+      // get transaction operation
+      const operations = await axios.get(
+        `${fundedTxResp._links.transaction.href}/operations`,
+      );
+      expect(operations.data._embedded.records.length).to.equal(4);
+      expect(operations.data._embedded.records[0].to).to.equal(escrowPubKey);
+      expect(operations.data._embedded.records[0].from).to.equal(userPubKey);
+      expect(operations.data._embedded.records[0].amount).to.equal('3.0000000');
+    });
+  });
 
-  // describe('Claim', () => {
-  //   it('should create escrow account, fund and claim', async () => {
-  //     // get initial server account balance
-  //     const preClaimServerBalance = await htlc
-  //       .accountInfo(serverPubKey)
-  //       .then(resp => Number(resp.balances[0].balance));
-  //     const serverKeyPair = stellarSdk.Keypair.fromSecret(serverSecret);
-  //     // server creates envelope to broadcast
-  //     const { createEnvelope, escrowPubKey } = await htlc.create(serverKeyPair);
-  //     // broadcast envelope
-  //     await htlc.broadcast(createEnvelope);
-  //     // create fund envelope, sends to user
-  //     const fundEnvelope = await htlc.fund(
-  //       serverKeyPair,
-  //       userPubKey,
-  //       escrowPubKey,
-  //       3,
-  //       hashX,
-  //     );
-  //     // user signs fund envelope
-  //     const userKeyPair = stellarSdk.Keypair.fromSecret(userSecret);
-  //     const signedFundEnvelope = htlc.sign(userKeyPair, fundEnvelope);
-  //     // broadcast funded envelope
-  //     await htlc.broadcast(signedFundEnvelope);
-  //     // server pays invoice and gets preimage
-  //     const claimEnvelope = await htlc.claim(
-  //       serverKeyPair,
-  //       escrowPubKey,
-  //       preimage,
-  //     );
-  //     // broadcast claim Envelope
-  //     await htlc.broadcast(claimEnvelope);
-  //     // get balance after claiming
-  //     const postClaimServerBalance = await htlc
-  //       .accountInfo(serverPubKey)
-  //       .then(resp => Number(resp.balances[0].balance));
-  //     // server should recieve XLM
-  //     expect(postClaimServerBalance).to.be.above(preClaimServerBalance);
-  //   });
-  // });
+  describe('Claim', () => {
+    it('should create escrow account, fund and claim', async () => {
+      // get initial server account balance
+      const preClaimServerBalance = await htlc
+        .accountInfo(serverPubKey)
+        .then(resp => Number(resp.balances[0].balance));
+      const serverKeyPair = stellarSdk.Keypair.fromSecret(serverSecret);
+      // server creates envelope to broadcast
+      const { createEnvelope, escrowPubKey } = await htlc.create(serverKeyPair);
+      // broadcast envelope
+      await htlc.broadcast(createEnvelope);
+      // create fund envelope, sends to user
+      const fundEnvelope = await htlc.fund(
+        serverKeyPair,
+        userPubKey,
+        escrowPubKey,
+        3,
+        hashX,
+      );
+      // user signs fund envelope
+      const userKeyPair = stellarSdk.Keypair.fromSecret(userSecret);
+      const signedFundEnvelope = htlc.sign(userKeyPair, fundEnvelope);
+      // broadcast funded envelope
+      await htlc.broadcast(signedFundEnvelope);
+      // server pays invoice and gets preimage
+      const claimEnvelope = await htlc.claim(
+        serverKeyPair,
+        escrowPubKey,
+        preimage,
+      );
+      // broadcast claim Envelope
+      await htlc.broadcast(claimEnvelope);
+      // get balance after claiming
+      const postClaimServerBalance = await htlc
+        .accountInfo(serverPubKey)
+        .then(resp => Number(resp.balances[0].balance));
+      // server should recieve XLM
+      expect(postClaimServerBalance).to.be.above(preClaimServerBalance);
+    });
+  });
 
   describe('Refund', () => {
     it('should create escrow account, fund and refund', async () => {
@@ -125,7 +125,7 @@ describe('Stellar HTLC - Stellar Network', () => {
         serverKeyPair,
         userPubKey,
         escrowPubKey,
-        999, // big spender
+        999, // big baller
         hashX,
       );
       // create refund envelope in case shit goes wrong
@@ -134,7 +134,7 @@ describe('Stellar HTLC - Stellar Network', () => {
         userPubKey,
         escrowPubKey,
         1,
-      ); // 1 second timelock
+      );
       // user signs fund envelope
       const userKeyPair = stellarSdk.Keypair.fromSecret(userSecret);
       const signedFundEnvelope = htlc.sign(userKeyPair, fundEnvelope);
