35390d54d505a117c960d36b81e1b2e2dcfdbc66
leon-do <leondo123@gmail.com>
2018-12-20 10:24:27 -0700

    rm try catch

--- packages/htlc/src/network-models/stellar/stellar-htlc.ts
+++ packages/htlc/src/network-models/stellar/stellar-htlc.ts
@@ -83,13 +83,9 @@ export class StellarHtlc<N extends Network> extends BaseHtlc<N> {
    * @param envelope
    */
   public async broadcast(envelope: string) {
-    try {
-      const txFromEnvelope = new stellarSdk.Transaction(envelope);
-      const resp = await this._server.submitTransaction(txFromEnvelope);
-      return resp;
-    } catch (err) {
-      throw new Error(err);
-    }
+    const txFromEnvelope = new stellarSdk.Transaction(envelope);
+    const resp = await this._server.submitTransaction(txFromEnvelope);
+    return resp;
   }
 
   /**
@@ -99,39 +95,34 @@ export class StellarHtlc<N extends Network> extends BaseHtlc<N> {
   public async create(
     keyPair: stellarSdk.Keypair,
   ): Promise<{ createEnvelope: string; escrowPubKey: string }> {
-    try {
-      // create a completely new and unique pair of keys
-      const escrowKeyPair = stellarSdk.Keypair.random();
-      const serverAccount = await this._server.loadAccount(keyPair.publicKey());
-      // build transaction with operations
-      const tb = new stellarSdk.TransactionBuilder(serverAccount)
-        .addOperation(
-          stellarSdk.Operation.createAccount({
-            destination: escrowKeyPair.publicKey(), // create escrow account
-            startingBalance: '2.00001', // 1 base + 0.5[base_reserve] per op(2) + tx fee (0.00001) => 2.00001 XLM minimum
-          }),
-        )
-        .addOperation(
-          stellarSdk.Operation.setOptions({
-            source: escrowKeyPair.publicKey(),
-            signer: {
-              ed25519PublicKey: keyPair.publicKey(), // add radar as signer on escrow account
-              weight: 1,
-            },
-          }),
-        );
+    // create a completely new and unique pair of keys
+    const escrowKeyPair = stellarSdk.Keypair.random();
+    const serverAccount = await this._server.loadAccount(keyPair.publicKey());
+    // build transaction with operations
+    const tb = new stellarSdk.TransactionBuilder(serverAccount)
+      .addOperation(
+        stellarSdk.Operation.createAccount({
+          destination: escrowKeyPair.publicKey(), // create escrow account
+          startingBalance: '2.00001', // 1 base + 0.5[base_reserve] per op(2) + tx fee (0.00001) => 2.00001 XLM minimum
+        }),
+      )
+      .addOperation(
+        stellarSdk.Operation.setOptions({
+          source: escrowKeyPair.publicKey(),
+          signer: {
+            ed25519PublicKey: keyPair.publicKey(), // add radar as signer on escrow account
+            weight: 1,
+          },
+        }),
+      );
 
-      // build and sign transaction
-      const tx = tb.build();
-      tx.sign(escrowKeyPair);
-      tx.sign(keyPair);
-      return {
-        createEnvelope: tx.toEnvelope().toXDR('base64'),
-        escrowPubKey: escrowKeyPair.publicKey(),
-      };
-    } catch (err) {
-      throw new Error(err);
-    }
+    // build and sign transaction
+    const tx = tb.build();
+    tx.sign(escrowKeyPair, keyPair);
+    return {
+      createEnvelope: tx.toEnvelope().toXDR('base64'),
+      escrowPubKey: escrowKeyPair.publicKey(),
+    };
   }
 
   /**
@@ -145,25 +136,21 @@ export class StellarHtlc<N extends Network> extends BaseHtlc<N> {
     escrowPubKey: string,
     preimage: string,
   ): Promise<string> {
-    try {
-      // load escrow account from pub key
-      const escrowAccount = await this._server.loadAccount(escrowPubKey);
-      // build claim transaction
-      const tb = new stellarSdk.TransactionBuilder(escrowAccount).addOperation(
-        stellarSdk.Operation.accountMerge({
-          destination: keyPair.publicKey(),
-        }),
-      );
+    // load escrow account from pub key
+    const escrowAccount = await this._server.loadAccount(escrowPubKey);
+    // build claim transaction
+    const tb = new stellarSdk.TransactionBuilder(escrowAccount).addOperation(
+      stellarSdk.Operation.accountMerge({
+        destination: keyPair.publicKey(),
+      }),
+    );
 
-      // build and sign transaction
-      const tx = tb.build();
-      tx.sign(keyPair);
-      // https://stellar.github.io/js-stellar-sdk/Transaction.html#signHashX
-      tx.signHashX(preimage);
-      return tx.toEnvelope().toXDR('base64');
-    } catch (err) {
-      throw new Error(err);
-    }
+    // build and sign transaction
+    const tx = tb.build();
+    tx.sign(keyPair);
+    // https://stellar.github.io/js-stellar-sdk/Transaction.html#signHashX
+    tx.signHashX(preimage);
+    return tx.toEnvelope().toXDR('base64');
   }
 
   /**
@@ -181,57 +168,53 @@ export class StellarHtlc<N extends Network> extends BaseHtlc<N> {
     amount: number,
     hashX: string,
   ): Promise<string> {
-    try {
-      // load account to sign with
-      const account = await this._server.loadAccount(userPubKey);
-      // add a payment operation to the transaction
-      const tb = new stellarSdk.TransactionBuilder(account)
-        .addOperation(
-          stellarSdk.Operation.payment({
-            amount: amount.toString(), // user pays amount
-            destination: escrowPubKey,
-            asset: stellarSdk.Asset.native(),
-          }),
-        )
-        .addOperation(
-          stellarSdk.Operation.setOptions({
-            source: escrowPubKey,
-            signer: {
-              ed25519PublicKey: keyPair.publicKey(), // add radar as signer on escrow account
-              weight: 1,
-            },
-          }),
-        )
-        .addOperation(
-          stellarSdk.Operation.setOptions({
-            source: escrowPubKey,
-            signer: {
-              ed25519PublicKey: userPubKey, // add user as signer on escrow account
-              weight: 1,
-            },
-          }),
-        )
-        .addOperation(
-          stellarSdk.Operation.setOptions({
-            source: escrowPubKey,
-            signer: {
-              sha256Hash: hashX, // add hash(x) as signer on escrow acount
-              weight: 1,
-            },
-            masterWeight: 0, // escrow cannot sign its own txs
-            lowThreshold: 2, // and add signing thresholds (2 signatures required)
-            medThreshold: 2,
-            highThreshold: 2,
-          }),
-        );
+    // load account to sign with
+    const account = await this._server.loadAccount(userPubKey);
+    // add a payment operation to the transaction
+    const tb = new stellarSdk.TransactionBuilder(account)
+      .addOperation(
+        stellarSdk.Operation.payment({
+          amount: amount.toString(), // user pays amount
+          destination: escrowPubKey,
+          asset: stellarSdk.Asset.native(),
+        }),
+      )
+      .addOperation(
+        stellarSdk.Operation.setOptions({
+          source: escrowPubKey,
+          signer: {
+            ed25519PublicKey: keyPair.publicKey(), // add radar as signer on escrow account
+            weight: 1,
+          },
+        }),
+      )
+      .addOperation(
+        stellarSdk.Operation.setOptions({
+          source: escrowPubKey,
+          signer: {
+            ed25519PublicKey: userPubKey, // add user as signer on escrow account
+            weight: 1,
+          },
+        }),
+      )
+      .addOperation(
+        stellarSdk.Operation.setOptions({
+          source: escrowPubKey,
+          signer: {
+            sha256Hash: hashX, // add hash(x) as signer on escrow acount
+            weight: 1,
+          },
+          masterWeight: 0, // escrow cannot sign its own txs
+          lowThreshold: 2, // and add signing thresholds (2 signatures required)
+          medThreshold: 2,
+          highThreshold: 2,
+        }),
+      );
 
-      // build and sign transaction
-      const tx = tb.build();
-      tx.sign(keyPair);
-      return tx.toEnvelope().toXDR('base64');
-    } catch (err) {
-      throw new Error(err);
-    }
+    // build and sign transaction
+    const tx = tb.build();
+    tx.sign(keyPair);
+    return tx.toEnvelope().toXDR('base64');
   }
 
   /**
@@ -247,36 +230,32 @@ export class StellarHtlc<N extends Network> extends BaseHtlc<N> {
     escrowPubKey: string,
     timelockSeconds: number,
   ): Promise<string> {
-    try {
-      // load escrow account from pub key
-      const escrowAccount = await this._server.loadAccount(escrowPubKey);
-      // build claim transaction with timelock
-      const tb = new stellarSdk.TransactionBuilder(escrowAccount, {
-        timebounds: {
-          minTime: Math.floor(Date.now() / 1000) + timelockSeconds,
-          maxTime: 0,
-        },
-      })
-        .addOperation(
-          stellarSdk.Operation.payment({
-            destination: keyPair.publicKey(),
-            asset: stellarSdk.Asset.native(),
-            amount: '2.00001', // send upfront cost back to radar
-          }),
-        )
-        .addOperation(
-          stellarSdk.Operation.accountMerge({
-            destination: userPubKey, // merge remainder back to user
-          }),
-        );
-      // build and sign transaction
-      const tx = tb.build();
-      tx.sign(keyPair);
-      // https://www.stellar.org/developers/horizon/reference/xdr.html
-      return tx.toEnvelope().toXDR('base64');
-    } catch (err) {
-      throw new Error(err);
-    }
+    // load escrow account from pub key
+    const escrowAccount = await this._server.loadAccount(escrowPubKey);
+    // build claim transaction with timelock
+    const tb = new stellarSdk.TransactionBuilder(escrowAccount, {
+      timebounds: {
+        minTime: Math.floor(Date.now() / 1000) + timelockSeconds,
+        maxTime: 0,
+      },
+    })
+      .addOperation(
+        stellarSdk.Operation.payment({
+          destination: keyPair.publicKey(),
+          asset: stellarSdk.Asset.native(),
+          amount: '2.00001', // send upfront cost back to radar
+        }),
+      )
+      .addOperation(
+        stellarSdk.Operation.accountMerge({
+          destination: userPubKey, // merge remainder back to user
+        }),
+      );
+    // build and sign transaction
+    const tx = tb.build();
+    tx.sign(keyPair);
+    // https://www.stellar.org/developers/horizon/reference/xdr.html
+    return tx.toEnvelope().toXDR('base64');
   }
 
   /**
@@ -285,12 +264,8 @@ export class StellarHtlc<N extends Network> extends BaseHtlc<N> {
    * @param envelope
    */
   public sign(keyPair: stellarSdk.Keypair, envelope: string): string {
-    try {
-      const tx = new stellarSdk.Transaction(envelope);
-      tx.sign(keyPair);
-      return tx.toEnvelope().toXDR('base64');
-    } catch (err) {
-      throw new Error(err);
-    }
+    const tx = new stellarSdk.Transaction(envelope);
+    tx.sign(keyPair);
+    return tx.toEnvelope().toXDR('base64');
   }
 }
