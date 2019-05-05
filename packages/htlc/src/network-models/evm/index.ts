f80351fda46d0d5d30dec5f53882db916bb59fd2
Cavan <cavan@radarrelay.com>
2019-05-05 16:02:22 -0600

    Export bytecode, bump htlc package version

--- packages/htlc/src/network-models/evm/index.ts
+++ packages/htlc/src/network-models/evm/index.ts
@@ -1,4 +1,10 @@
 export * from './evm-htlc';
 export * from './contract-addresses';
-export { abi as ERC20SwapABI } from './contract-artifacts/ERC20Swap.json';
-export { abi as EtherSwapABI } from './contract-artifacts/EtherSwap.json';
+export {
+  abi as ERC20SwapABI,
+  bytecode as ERC20SwapBytecode,
+} from './contract-artifacts/ERC20Swap.json';
+export {
+  abi as EtherSwapABI,
+  bytecode as EtherSwapBytecode,
+} from './contract-artifacts/EtherSwap.json';
