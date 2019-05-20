13742030e13a2c7c816a7136eec208d51a83be3d
Cavan <cavan@radarrelay.com>
2019-05-20 14:11:43 -0600

    Fix typo

--- packages/redshift-types/src/constants/general.ts
+++ packages/redshift-types/src/constants/general.ts
@@ -20,7 +20,7 @@ export enum InternalSwapState {
   WAITING_FOR_REFUND_TX_CONFIRMATION = 'WaitingForRefundTxConfirmation',
   REFUNDED = 'Refunded',
 
-  FUND_WINDOW_ELAPSED = 'FundWindowElapsed', // The fund window had elapsed
+  FUND_WINDOW_ELAPSED = 'FundWindowElapsed', // The fund window has elapsed
   FUNDED_BY_OTHER_QUOTE = 'FundedByOtherQuote', // Another quote for the same invoice has been funded
 }
 
@@ -40,7 +40,7 @@ export enum UserSwapState {
   WAITING_FOR_REFUND_TX_CONFIRMATION = 'WaitingForRefundTxConfirmation',
   REFUNDED = 'Refunded',
 
-  FUND_WINDOW_ELAPSED = 'FundWindowElapsed', // The fund window had elapsed
+  FUND_WINDOW_ELAPSED = 'FundWindowElapsed', // The fund window has elapsed
   FUNDED_BY_OTHER_QUOTE = 'FundedByOtherQuote', // Another quote for the same invoice has been funded
 }
 
