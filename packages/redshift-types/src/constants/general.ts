cd70326c96217149f834e35f1a42e2415005ca98
Cavan <cavan@radarrelay.com>
2019-08-28 12:51:32 -0600

    RED-198: Rename endpoint

--- packages/redshift-types/src/constants/general.ts
+++ packages/redshift-types/src/constants/general.ts
@@ -218,7 +218,7 @@ export namespace Ws {
     UNSUBSCRIBE_FROM_BLOCK_HEIGHT = 'unsubscribeFromBlockHeight',
     BROADCAST_TRANSACTION = 'broadcastTransaction',
     REQUEST_REFUND_DETAILS = 'requestRefundDetails',
-    REQUEST_QUOTE_REQUEST_REQUIREMENTS = 'requestQuoteRequestRequirements',
+    REQUEST_MARKET_REQUIREMENTS = 'requestMarketRequirements',
     // CLIENT
     MAKER_QUOTE = 'makerQuote',
     STATE_CHANGED = 'stateChanged',
