3027e95c7f1a7d88918f4137b3d845f395245284
Cavan <cavan@radarrelay.com>
2020-01-22 14:09:30 -0700

    Add invalid token contract address enum member

--- packages/redshift-types/src/constants/error.ts
+++ packages/redshift-types/src/constants/error.ts
@@ -81,6 +81,7 @@ export enum LiquidityProviderApiError {
   INVALID_PAY_TO_ADDRESS = 'InvalidPayToAddress',
   INVALID_FUND_TX_VALUE = 'InvalidFundTxValue',
   INVALID_CONTRACT_DATA = 'InvalidContractData',
+  INVALID_TOKEN_CONTRACT_ADDRESS = 'InvalidTokenContractAddress',
   INVALID_SUCCESS_VALUE = 'InvalidSuccessValue',
   INVALID_PREIMAGE = 'InvalidPreimage',
   CONTRACT_IS_NOT_WHITELISTED = 'ContractIsNotWhitelisted',
